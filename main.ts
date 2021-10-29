155c653652dae1e90d0e94f9e4840705770f137d
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-10-29 22:45:53 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-let Coeur = 0
+basic.showString("Magie !")
+let Lumiere = input.lightLevel()
+let Coeur = 1
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    if (input.lightLevel() < 30) {
+    if (input.lightLevel() < Lumiere) {
         if (Coeur == 0) {
             basic.showIcon(IconNames.Heart)
             Coeur = 1
