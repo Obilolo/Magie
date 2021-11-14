c7b679baa6cb609525be6f459188772f8c7befda
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-11-14 18:06:33 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Ghost)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.TShirt)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    basic.clearScreen()
+let Rapport = 0
+basic.forever(function () {
+    while (Rapport < 1023) {
+        pins.digitalWritePin(DigitalPin.P0, Rapport)
+        Rapport += 1
+        basic.pause(10)
+    }
+    while (Rapport > 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        Rapport += -1
+        basic.pause(10)
+    }
 })
