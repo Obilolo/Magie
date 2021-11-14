f76abe17c154089b939cfdc216caba63cd0da727
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-11-14 18:11:41 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ let Rapport = 0
 basic.forever(function () {
     while (Rapport < 1023) {
         pins.digitalWritePin(DigitalPin.P0, Rapport)
-        Rapport += 1
+        Rapport = Rapport + 1
         basic.pause(10)
     }
     while (Rapport > 0) {
         pins.digitalWritePin(DigitalPin.P0, 0)
-        Rapport += -1
+        Rapport = Rapport - 1
         basic.pause(10)
     }
 })
