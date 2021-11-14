bf698d2ca81879eebf3c49cca5119c3e66a3c4c4
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-11-14 18:20:43 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ input.onButtonPressed(Button.A, function () {
     if (Rapport > 0) {
         Rapport += -10
         pins.digitalWritePin(DigitalPin.P0, Rapport)
+        basic.showString("" + (Rapport))
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (Rapport < 1020) {
         Rapport += 10
         pins.digitalWritePin(DigitalPin.P0, Rapport)
+        basic.showString("" + (Rapport))
     }
 })
 let Rapport = 0
