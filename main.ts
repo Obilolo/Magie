1740161f279c3bfd1698a18897c61dfd8d5b7328
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-12-28 16:14:23 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    if (Rapport > 0) {
-        Rapport += -1
+    if (Rapport >= 10) {
+        Rapport += -10
         pins.digitalWritePin(DigitalPin.P0, Rapport)
-        basic.showString("" + Rapport / 1)
+        basic.showString("" + (Rapport / 10))
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (Rapport < 20) {
-        Rapport += 1
+    if (Rapport < 1013) {
+        Rapport += 10
         pins.digitalWritePin(DigitalPin.P0, Rapport)
-        basic.showString("" + Rapport / 1)
+        basic.showString("" + (Rapport / 10))
     }
 })
 let Rapport = 0
