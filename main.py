3580eb7b6d7b113a3cd20b9317a956751e9701b7
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-12-28 16:08:44 +0100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 def on_button_pressed_a():
-    global Lumiere
-    Lumiere += -1
-    basic.show_string("" + str((Lumiere)))
+    global Rapport
+    if Rapport > 0:
+        Rapport += -1
+        pins.digital_write_pin(DigitalPin.P0, Rapport)
+        basic.show_string("" + str((Rapport / 1)))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global Lumiere
-    Lumiere += 1
-    basic.show_string("" + str((Lumiere)))
+    global Rapport
+    if Rapport < 20:
+        Rapport += 1
+        pins.digital_write_pin(DigitalPin.P0, Rapport)
+        basic.show_string("" + str((Rapport / 1)))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-Lumiere = 0
-basic.show_string("Magie !")
-Lumiere = input.light_level()
-Coeur = 1
-basic.show_string("" + str((Lumiere)))
-basic.show_icon(IconNames.HEART)
-
-def on_forever():
-    global Coeur
-    if input.light_level() < Lumiere:
-        if Coeur == 0:
-            basic.show_icon(IconNames.HEART)
-            Coeur = 1
-        else:
-            basic.show_leds("""
-                . . . . .
-                                . . . . .
-                                . . . . .
-                                . . . . .
-                                . . . . .
-            """)
-            Coeur = 0
-basic.forever(on_forever)
+Rapport = 0
+Rapport = 0
--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     if (Rapport > 0) {
-        Rapport += -10
+        Rapport += -1
         pins.digitalWritePin(DigitalPin.P0, Rapport)
-        basic.showString("" + (Rapport))
+        basic.showString("" + Rapport / 1)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (Rapport < 1020) {
-        Rapport += 10
+    if (Rapport < 20) {
+        Rapport += 1
         pins.digitalWritePin(DigitalPin.P0, Rapport)
-        basic.showString("" + (Rapport))
+        basic.showString("" + Rapport / 1)
     }
 })
 let Rapport = 0
