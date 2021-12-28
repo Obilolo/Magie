55ac1a9ae3eb51df6255b602058cbf0d8c4c6ea4
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-12-28 16:17:01 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    Rapport = 1000
+})
 input.onButtonPressed(Button.A, function () {
     if (Rapport >= 10) {
         Rapport += -10
         pins.digitalWritePin(DigitalPin.P0, Rapport)
         basic.showString("" + (Rapport / 10))
     }
+})
+input.onButtonPressed(Button.AB, function () {
+    Rapport = 0
 })
 input.onButtonPressed(Button.B, function () {
     if (Rapport < 1013) {
